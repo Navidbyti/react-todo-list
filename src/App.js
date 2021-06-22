cba7e026db632566baa43bfcdf5ce032188a5dcd
Navid Bayati <nbayati22@gmail.com>
2021-06-22 21:12:15 +0430

    just a placeholder for now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 function App(){
     return(
   
-        <h1>React TODO List!</h1>
+        <div>
+            <input type="checkbox" id='todo1' />
+            <lable for='todo1'>Random task 1</lable><br/>
+
+            <input type="checkbox" id='todo2' />
+            <lable for='todo2'>Random task 2</lable><br/>
+
+            <input type="checkbox" id='todo3' />
+            <lable for='todo3'>Random task 3</lable><br/>
+            
+            <input type="checkbox" id='todo4' />
+            <lable for='todo4'>Random task 4</lable><br/>
+
+        </div>
     )
 };
 
