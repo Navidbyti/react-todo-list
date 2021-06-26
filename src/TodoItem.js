fb52794b601e5bffab46a13811af905748bd6bed
unknown <nbayati22@gmail.com>
2021-06-26 14:51:41 +0430

    added edit and delet with states

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,51 @@
 
+import { useState } from "react";
 import "./todoitem.css";
 
-function TodoItem({id, isComplete, text ,removeTodo}) {
-  let isChecked;
-  
-  
+function TodoItem({ id, isComplete, text, removeTodo }) {
+  const [checked, setChecked] = useState(isComplete);
+  const [editField, setEditField] = useState(text);
+  function loadEditField(e) {
+    setEditField(
+      <input
+        type="text"
 
-  
+        onKeyPress={(e) => {
+            const {value} = e.target
+          if (e.key === "Enter") {
+              
+            setEditField(value);
+          }
+        }}
+      />
+    );
+  }
   return (
     <div className="todo-container">
-      <p>TODO: {text}</p>
-      <button onClick={() => {
-         removeTodo(id);
-         console.log(id)
-      }}>Delete</button>
+      <p className={checked ? "active" : ""}>TODO: {editField}</p>
+
+      <button
+        onClick={() => {
+          removeTodo(id);
+        }}
+      >
+        Delete
+      </button>
+      <button
+        onClick={(e) => {
+          loadEditField(e);
+        }}
+      >
+        Edit
+      </button>
+
       <input
         type="checkbox"
         onChange={() => {
-          isChecked = !isComplete;
+          setChecked(!checked);
+          console.log(checked);
         }}
-        checked={isChecked}
+        checked={checked}
       />
     </div>
   );
